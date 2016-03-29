bc868aa684d8d6862bfd34026f82d478dc3da091
David Helmer <dhelmer@mavensconsulting.com>
2016-03-29 18:13:42 -0400

    promisfying and testing accessing redis

--- status-redis-store.js
+++ status-redis-store.js
@@ -1,9 +1,16 @@
+
 function StatusRedisStore(redisClient){
     this.redisClient = redisClient;
 }
 
 StatusRedisStore.prototype.getStatus = function(){
-    
-}
+    return this.redisClient.getAsync('status').then(function(result){
+        return JSON.parse(result);
+    });
+};
+
+StatusRedisStore.prototype.setStatus = function(status){
+    return this.redisClient.setAsync('status', JSON.stringify(status));
+};
 
 module.exports = StatusRedisStore;
--- test/status-redis-store.js
+++ test/status-redis-store.js
@@ -1,18 +1,68 @@
 var expect = require('expect.js');
 var StatusRedisStore = require('../status-redis-store.js');
+var redis = require('redis-mock');
+var redisClient = redis.createClient();
+var Bluebird = require('bluebird');
+var basicData = { testData: 'blahhhh' };
 
-describe('statusRedisStore', testStatusRedisStore);
+Bluebird.promisifyAll(redis.RedisClient.prototype);
+
+describe('statusRedisStore with empty redis', testStatusRedisStore);
 
 function testStatusRedisStore(){
-    var statusRedisStore = new StatusRedisStore();
+    beforeEach(flushRedis);
+    afterEach(flushRedis);
+    var statusRedisStore = new StatusRedisStore(redisClient);
     
     describe('getStatus', testGetStatus(statusRedisStore));
+    describe('setStatus', testSetStatus(statusRedisStore));
+}
+
+function flushRedis(){
+    redisClient.flushdb();
 }
 
 function testGetStatus(statusRedisStore){
     return function(){
         it('exists as a public method', function(){
             expect(typeof statusRedisStore.getStatus).to.eql('function');
-        });  
+        });
+        
+        it('returns null when empty', function(done){
+            statusRedisStore.getStatus().done(function(status){
+                expect(status).to.eql(null);
+                
+                done(); 
+            });
+        });
+        
+        it('returns data if there', function(done){
+            
+            redisClient.setAsync('status', JSON.stringify(basicData))
+            .done(function(){
+                statusRedisStore.getStatus().done(function(status){
+                    expect(status).to.eql(basicData);
+                    done(); 
+                });
+            });
+        });
+    };
+}
+
+function testSetStatus(statusRedisStore){
+    return function(){
+        it('exists as a public method', function(){
+            expect(typeof statusRedisStore.setStatus).to.eql('function');
+        });
+        
+        it('sets the status', function(done){
+            statusRedisStore.setStatus(basicData).done(function(){
+                redisClient.getAsync('status')
+                .done(function(status){
+                    expect(JSON.parse(status)).to.eql(basicData);
+                    done();
+                });
+            });
+        });
     };
 }
