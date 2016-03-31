0af8bf755e32fe2f82793b93f10e018803eb0978
David Helmer <dhelmer@mavensconsulting.com>
2016-03-31 08:20:16 -0400

    simplify overloading the environment

--- overload-environment.js
+++ overload-environment.js
@@ -1,6 +1,6 @@
 var environmentFile = '.env';
 try{
-    var env = require('node-env-file')(__dirname + '/' + environmentFile);
+    require('node-env-file')(__dirname + '/' + environmentFile);
 } catch(envLoadException){
     console.log('No environment variable overloads available at ' + environmentFile);
 }
