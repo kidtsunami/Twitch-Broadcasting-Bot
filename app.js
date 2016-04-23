b724a3f2e4d6153cc6f38fd9dcc78fa73c078e3c
David Helmer <dhelmer@mavensconsulting.com>
2016-04-23 10:08:00 -0400

    adding in body parsing for commands

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+
 app.use(require('express-promise')());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/command', function(request, response){
-  console.log(request.query);
   if(request.query.check_ssl == '1'){
     response.sendStatus(200);
   } else {
     response.status(404).send('You must be lost');
   }
+});
+
+
+app.post('/command', function(request, response){
+  console.log(request.body);
+  response.sendStatus(200);
 });
 
 var server = app.listen(3000, function(){
